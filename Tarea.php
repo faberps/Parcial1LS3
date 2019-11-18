46ddf33ba1b2cea41b6b33c0bae4e30d8043559c
CristianManzano <Manzanocristian1104@gmail.com>
2019-11-17 23:59:34 -0500

    Ahora si Prro

--- Tarea.php
+++ Tarea.php
@@ -16,23 +16,19 @@
   </head>
   <body>
     
-  <nav class="navbar  fixed-top navbar-expand-md navbar-light bg-light">
-    <a class="navbar-brand " href="#">
-      <img src="img/logo2.png" width="30" height="30" class="align-top" alt="">
-      WeekPlan
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><h3><img src="img/logo2.png" width="50" height="50" class="align-center" alt="">
+                WeekPlan</h3></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-    <div class="navbar-collapse collapse" id="navbarSupportedContent">
-
-        <ul class="navbar-nav ml-auto">
-                <a class="nav-link" href="index.html" data-target="#myModal" data-toggle="modal">SALIR</a>
-            
-        </ul>
-    </div>
-  </nav>
+      
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                <label> <a href="index.php">Cerrar Sesion</a> </label> 
+            </div>
+    </nav>
     <div class="container">
       <div class="centrado">
         <h2>¡Bienvedido!</h2>
